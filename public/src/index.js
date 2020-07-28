125b00a5b4b85738098896055d2b1f34a46c808a
Ronen Raduzkym <rraduzky@gmail.com>
2020-07-28 12:38:59 +0100

    Completed contacts and locate sections.

---
+++ public/src/index.js
@@ -0,0 +1,9 @@
+import Typed from "typed.js";
+
+var output = new Typed("#output", {
+  strings: ["HUNGRY?", "CURIOUS?", "BRAVE?"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+});
