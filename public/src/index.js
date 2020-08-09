9d43b062dd58d1487e4ba028dca82a3550a24d8f
Ronen Raduzkym <rraduzky@gmail.com>
2020-08-09 22:08:15 +0100

    Type animation working

---
+++ public/src/index.js
@@ -0,0 +1,22 @@
+console.log("hello");
+
+import TypeIt from "typeit";
+
+// Type animation.
+let options = {
+  speed: 100,
+  loop: true,
+  cursor: false,
+};
+
+let type = new TypeIt("#output", options)
+  .go()
+  .type("HUNGRY?")
+  .pause(2000)
+  .delete()
+  .type("BRAVE?")
+  .pause(2000)
+  .delete()
+  .type("CURIOUS?")
+  .pause(2000)
+  .delete();
